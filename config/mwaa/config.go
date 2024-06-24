d7a0e743e92e8e9e48c7458954f3d7800db21283
sanhaKimVics <sanha.kim@vicsnc.com>
2024-06-25 10:17:32 +1200

    editing config.go and updating provider.go
    Signed-off-by: sanhaKimVics <sanha.kim@vicsnc.com>

--- config/mwaa/config.go
+++ config/mwaa/config.go
@@ -1,36 +1,22 @@
+// SPDX-FileCopyrightText: 2024 The Crossplane Authors <https://crossplane.io>
+//
+// SPDX-License-Identifier: CC0-1.0
+
 package mwaa
 
 import (
 	"github.com/crossplane/upjet/pkg/config"
-
-	"github.com/upbound/provider-aws/config/common"
 )
 
 // Configure adds configurations for mwaa group.
 func Configure(p *config.Provider) {
-
 	p.AddResourceConfigurator("aws_mwaa_environment", func(r *config.Resource) {
-		r.References = config.References{
-			"execution_role_arn": config.Reference{
-				TerraformName: "aws_iam_role",
-				Extractor:     common.PathARNExtractor,
-			},
-			"network_configuration.subnet_ids": config.Reference{
-				TerraformName:     "aws_subnet",
-				RefFieldName:      "SubnetIDRefs",
-				SelectorFieldName: "SubnetIDSelector",
-			},
-			"network_configuration.security_groups": config.Reference{
-				TerraformName:     "aws_security_group",
-				RefFieldName:      "SecurityGroupRefs",
-				SelectorFieldName: "SecurityGroupSelector",
-			},
-			"source_bucket_arn": config.Reference{
-				TerraformName: "aws_s3_bucket",
-			},
+		r.References["network_configuration.subnet_ids"] = config.Reference{
+			TerraformName: "aws_subnet",
 		}
-		r.UseAsync = true
-
+		r.References["network_configuration.security_groups"] = config.Reference{
+			TerraformName: "aws_security_group",
+		}
 	})
 
 }
